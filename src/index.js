4a397ba7d3547333d8e2324e05152e38c9a3927a
Tatiana Tikhonova <tanianova@yandex.ru>
2022-01-26 19:16:49 +0300

    добавлена валидация формы

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import maskPrice from './modules/maskPrice';
-import './styles/index.scss'
+import maskPrice from "./modules/maskPrice";
+import createCard from "./modules/createCard";
+import checkInputValue from "./modules/checkInputValue";
+import "./styles/index.scss";
 
-window.addEventListener('DOMContentLoaded', () => {
-    'use strict';
-    maskPrice('[name=price]')
- 
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+  maskPrice("[name=price]");
+  checkInputValue('.page-content__form','.page-content__form .button')
+  
 });
-
---
+++ src/modules/checkInputValue.js
@@ -0,0 +1,40 @@
+const checkInputValue = (formSelector, submitSelector) => {
+  const submitButton = document.querySelector(submitSelector);
+  const form = document.querySelector(formSelector);
+
+  const addWarning = (input) => {
+    const warningSpan = document.createElement("span");
+    warningSpan.classList.add("invalid");   
+    warningSpan.textContent = "Поле является обязательным";
+
+    if (input.value === "" && !input.classList.contains("invalid-input")) {
+      input.classList.add("invalid-input");
+      input.after(warningSpan);
+    }
+  };
+
+  const removeWarning = (input) => {
+    input.parentElement.removeChild(document.querySelector(".invalid"));
+    input.classList.remove("invalid-input");
+  };
+
+  form.addEventListener("focusout", (e) => {
+    let target = e.target;
+    target.required && addWarning(target);
+  });
+
+  form.addEventListener("keyup", (e) => {
+    let target = e.target;
+    if (target.value.trim().length > 0) {
+      target.classList.contains("invalid-input") && removeWarning(target);
+      form.checkValidity()
+        ? (submitButton.disabled = false)
+        : (submitButton.disabled = true);
+    } else if (target.value.trim().length === 0 && target.required) {
+      addWarning(target, "Поле является обязательным");
+      submitButton.disabled = true;
+    }
+  });
+};
+
+export default checkInputValue;
